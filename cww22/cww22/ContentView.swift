065da84e1adc7a34333d124ad63de46013e30db4
Ralhaddad7 <112815184+Ralhaddad7@users.noreply.github.com>
2022-09-11 00:22:36 +0300

    cw2 done

---
+++ cww22/cww22/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  cww22
+//
+//  Created by Ritaj Alhaddad on 10/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var imageName = "5"
+    var body: some View {
+        VStack {
+            Text("ماهو مسار مفضل لديك ؟")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Image("5")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+            
+            Text("ios")
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 200, height: 60 )
+                .background(Color.black)
+                .padding()
+                .onTapGesture {
+                    imageName="4"
+                }
+            
+            Text("gamedev")
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 200, height: 60)
+                .background(Color.black)
+                .padding()
+                .onTapGesture {
+                    imageName="3"
+                }
+            
+            Text("web")
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 200, height: 60)
+                .background(Color.black)
+                .padding()
+                .onTapGesture {
+                    imageName="1"
+                }
+            
+            Text("android")
+                .font(.title)
+                .foregroundColor(.white)
+                .frame(width: 200, height: 60)
+                .background(Color.black)
+                .padding()
+                .onTapGesture {
+                    imageName="2"
+                }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cww22/cww22/cww22App.swift
@@ -0,0 +1,17 @@
+//
+//  cww22App.swift
+//  cww22
+//
+//  Created by Ritaj Alhaddad on 10/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct cww22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
